a713092bf7ae8d3920bce6f936aaedd6090c9f97
Lean <872082613@qq.com>
2017-04-17 22:21:36 +0800

    beautify my exp06

--- exp06.js
+++ exp06.js
@@ -4,16 +4,13 @@ var ind=0;
 function init()
 {
 	$(".ui-p1-cont2").hide();
-	$(".ui-p1-box").click(function(){
-		var num=$(this).index();
-		$(".ui-p1-bigger").text(num+1);
+	$(".ui-p1-box").click(function(e){
+		$(".ui-p1-bigger").attr("src",$(this)[0].src);
 		$(".ui-p1-cont2").show();
 	});
 	$(".ui-p1-cont2").click(function(){
 		$(".ui-p1-cont2").fadeOut(100);
 	});
-
-
 
 	$(".ui-p2-page-con").eq(1).hide();
 	$(".ui-p2-page-con").eq(2).hide();
@@ -30,7 +27,10 @@ function init()
 
 	$(".ui-p3-button").click(function(){
 		cnt=cnt+1;
-		$(".ui-p3-box:last").after("<div class='ui-p3-box'><div class='ui-p3-box-lab'></div><div class='ui-p3-box-button'>Detele</div></div>");
+		if(cnt==1)
+			$(".ui-p3-button").before("<div class='ui-p3-box'><div class='ui-p3-box-lab'></div><div class='ui-p3-box-button'>Detele</div></div>");
+		else
+			$(".ui-p3-box:last").after("<div class='ui-p3-box'><div class='ui-p3-box-lab'></div><div class='ui-p3-box-button'>Detele</div></div>");
 		$(".ui-p3-box-lab:last").text(cnt);
 		console.log(cnt);
 	});
